a8cb7196d7ad48e1857deca752dd9d251ae7e5d6
kenneth <knnthgb123@gmail.com>
2023-05-24 22:45:49 +0800

    JS practice

---
+++ main.js
@@ -0,0 +1,13 @@
+const a = 'Hi Kenneth';
+console.log(a.split('') );
+
+const age = [ 10, 2, 3];
+age[2] = 21 /*use to add/replace value*/
+age.push (30) /* to add value and place it to the end */
+console.log(age)
+
+
+
+
+
+
