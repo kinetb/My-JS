eac29ead314ed31ba6e7c906f89953cab1a95120
kenneth <knnthgb123@gmail.com>
2023-05-25 22:24:54 +0800

    I put different samples

--- main.js
+++ main.js
@@ -1,10 +1,63 @@
-const a = 'Hi Kenneth';
-console.log(a.split('') );
+/*const btn = document.querySelector(".btn");
+
+function myfunction(){
+    console.log("piwpiw")
+
+}
+
+myfunction()*/
+
+
+
+
+
 
 const age = [ 10, 2, 3];
 age[2] = 21 /*use to add/replace value*/
 age.push (30) /* to add value and place it to the end */
 console.log(age)
+
+const a = 'Hi Kenneth';
+console.log(a.split('') );
+
+
+const b = 'Hello my Friend';
+console.log(b.toUpperCase());
+
+const c = 'Hi my Friend';
+console.log(c.toLowerCase())
+
+
+const d = 'Hi my Friend';
+console.log(d.length)
+
+
+const e = 'Hi my Friend';
+console.log(e.substring(6, 12).toLowerCase())
+
+
+const random = ['kenneth', 'leo', 'shiela']
+console.log(random)
+
+
+const name = 'Kenneth';
+const number = '21'
+
+// concatenation
+console.log('My name is ' +name+ ' and I am ' +number);
+// Template string
+console.log(`My name is ${name} and I am ${number}`)
+
+const Me = {
+    firstName: 'Kenneth',
+    lastName: 'Pogi',
+    edad: '21',
+    address: 'Valenzuela'   
+}
+
+console.log(Me.edad);
+
+
 
 
 
